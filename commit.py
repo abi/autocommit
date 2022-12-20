873a535486f3d95e8c8029c3aa4af42eb2e8e460
Abi Raja <abimanyuraja@gmail.com>
2022-12-20 13:25:06 -0500

    Add error handling for when not in a git repo and other errors

--- commit.py
+++ commit.py
@@ -3,19 +3,22 @@ import pygit2
 
 from llm import generate_suggestions
 
-repo = pygit2.Repository(".")
+# repo = pygit2.Repository(".")
 
+#  Get the diff including untracked files (see https://stackoverflow.com/a/52093887)
 git_command = "git --no-pager diff; for next in $( git ls-files --others --exclude-standard ) ; do git --no-pager diff --no-index /dev/null $next; done;"
 output = subprocess.run(git_command, shell=True,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+if len(output.stderr) > 0:
+    error = output.stderr.decode("utf-8")
+    print("There was an error running the command: ")
+    if error.startswith("warning: Not a git repository"):
+        print("You're not inside a git repo. Please run it from inside a git repo.")
+    else:
+        print(error)
+    exit(-1)
+
 diff = output.stdout.decode("utf8")
-
-print("\n".join(generate_suggestions(diff)))
-
 # print(diff)
-
-# Handle errors here
-
-# print(output.stderr)
-# print(output.returncode)
+print("\n".join(generate_suggestions(diff)))
